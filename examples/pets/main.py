f60cc3f67239fbec7f5e7a1331a8ccac25607c69
Aaron <AaronBatilo@gmail.com>
2018-09-02 15:19:31 -0600

    Reduce sample code and make sample use sanic_attrs
    The original sanic-attrs project never converted the example to be
    sanic-attrs compatible. Until we rename/rebrand, I'll continue to use
    sanic-attrs for imports.

---
+++ examples/pets/main.py
@@ -0,0 +1,30 @@
+from sanic import Sanic
+
+from sanic_attrs import (
+    doc,
+    openapi_blueprint,
+    response,
+    swagger_blueprint
+)
+
+app = Sanic(__name__, strict_slashes=True)
+
+app.blueprint(openapi_blueprint)
+app.blueprint(swagger_blueprint)
+
+
+class Pet(doc.Model):
+    name: str = doc.field(description='Name of the pet')
+    age: int = doc.field(description='How old is the pet')
+
+
+@app.get('/')
+@doc.summary('List pets')
+@doc.produces(Pet)
+async def root(req):
+    pet = Pet('Chopper', 3)
+    return response.model(pet)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
