db31de0cc985f8fc4859a4b4fb92314a124a8320
Aaron <AaronBatilo@gmail.com>
2018-09-03 20:42:28 -0600

    Remove parser blueprint
    This blueprint didn't do anything and didn't appear to be used
    whatsoever. I'm not entirely sure what the intention was.

--- sanic_attrs/__init__.py
+++ sanic_attrs/__init__.py
@@ -1,6 +1,5 @@
 from .openapi import blueprint as openapi_blueprint
-from .parser import blueprint as parser_blueprint
 from .swagger import blueprint as swagger_blueprint
 
 __version__ = '0.2.1'
-__all__ = ['openapi_blueprint', 'parser_blueprint', 'swagger_blueprint']
+__all__ = ['openapi_blueprint', 'swagger_blueprint']
